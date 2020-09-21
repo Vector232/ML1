5f92dc219181744af2e0bea9e538fa961e7bd791
Vector232 <71130074+Vector232@users.noreply.github.com>
2020-09-21 15:29:23 +0300

    Update kNN.R

--- kNN/kNN.R
+++ kNN/kNN.R
@@ -6,7 +6,7 @@ sortObjectsByDist = function(xl, z){
   distances = matrix(NA, l, 2);
   
   for (i in 1:l){
-      distances[i, ] = c(i, sqrt(sum((xl[i, 1:n] - z)^2)));
+    distances[i, ] = c(i, sqrt(sum((xl[i, 1:n] - z)^2)));
   }
   
   orderedXl = xl[order(distances[, 2]), ];
@@ -15,13 +15,13 @@ sortObjectsByDist = function(xl, z){
 
 kNN = function(xl, z, k){
   orderedXl = sortObjectsByDist(xl, z);
-    
+  
   classes = orderedXl[1:k, 3];
-      
+  
   counts = table(classes);
-      
+  
   class = names(which.max(counts));
-
+  
   return (class)
 }
 
@@ -31,13 +31,17 @@ xl = iris[,3:5];
 colors <- c("setosa" = "red", "versicolor" = "green3", "virginica" = "blue")
 plot(iris[, 3:4], pch = 21, bg = colors[iris$Species], col = colors[iris$Species], asp = 1)
 
-for(i in 1:25){
-  x = runif(1, 0, 7);
-  y = runif(1, 0, 2.5);
-  z <- c(x, y);
-  class <- kNN(xl, z, k=6);
-  points(z[1], z[2], pch = 22, bg = colors[class], asp = 1);
-}
+x = 1;
+y = 0;
 
+while(x <= 7){
+  while(y <= 3){
+    class <- kNN(xl, c(x,y), k=6);
+    points(x, y, pch = 22, bg = colors[class], col = colors[class], asp = 1);
+    y = y + 0.5;
+  }
+  y = 0;
+  x = x + 0.5;
+}
 
 
