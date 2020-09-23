d58442ef090b469c424a96921e0c40b8f84a1e85
Vector232 <71130074+Vector232@users.noreply.github.com>
2020-09-23 11:47:44 +0300

    Update kwNNLOO_2.R

--- kwNNLOO/kwNNLOO_2.R
+++ kwNNLOO/kwNNLOO_2.R
@@ -1,17 +1,12 @@
-sortObjectsByDist = function(xl, k){
+sortObjectsByDist = function(xl, z){
   
   l = dim(xl)[1];
-  z = xl[k, 1:2];
+  n = dim(xl)[2]-1;
   
-  distances = matrix(NA, l-1, 2);
-  
-  c = 1;
+  distances = matrix(NA, l, 2);
   
   for (i in 1:l){
-    if(i != k ){
-      distances[c, ] = c(sqrt(sum((xl[i, 1:2] - z)^2)), xl[i, 3]);
-      c = c + 1;
-    }
+    distances[i, ] = c(sqrt(sum((xl[i, 1:n] - z)^2)), xl[i, 3]);
   }
   
   orderedXl = distances[order(distances[, 1]),];
@@ -19,37 +14,46 @@ sortObjectsByDist = function(xl, k){
   return (orderedXl);
 }
 
-LOO = function(xl, q){
-  n = dim(xl)[1] - 1;
-  DataA = array(0,n);
+classif_kwNN = function(orderedXl, k, q){
   
+  weightClasses = table(orderedXl[,2]);
+  weightClasses[1:dim(weightClasses)] = 0
+
+  for(j in 1:k){
+    weightClasses[orderedXl[j,2]] = weightClasses[orderedXl[j,2]] + (orderedXl[j,1] * q^j);
+    
+  }
+  
+  class = names(which.max(weightClasses));
+  
+  return (class)
+}
+
+kwNN = function(xl, z, k, q){
+  return(classif_kwNN(sortObjectsByDist(xl, z), k, q));
+}
+
+LOO = function(xl, q, sort = sortObjectsByDist, classification = classif_kwNN){
+  n = dim(xl)[1]-1;
+  DataA = array(0,n);
+  # print(n)
   for(i in 1:n){
-    
-    orderedXl = sortObjectsByDist(xl, i);
-    
+    orderedXl = sort(xl[-i,], c(xl[i,1], xl[i,2]));
     
     for(k in 1:n){
-      weightClasses = table(xl[0, 3]);
-      
-      for(j in 1:k){
-        #print(j);
-        if(orderedXl[j,2] == 1){ weightClasses[1] = weightClasses[1] + (orderedXl[j,1] * q^j);}
-        else if(orderedXl[j,2] == 2){ weightClasses[2] = weightClasses[2] + (orderedXl[j,1] * q^j);}
-        else if(orderedXl[j,2] == 3){ weightClasses[3] = weightClasses[3] + (orderedXl[j,1] * q^j);}
-      }
-      
-      class = names(which.max(weightClasses));
-      
-      if(class != xl[i,3]){
+      class = classification(orderedXl, k, q);
+      if(class != c(xl[i,3])){
+        
+        #print(c(xl[i,3]))
         DataA[k] = DataA[k] + 1;
       }
     }
     print(i);
     print(DataA);
   }
-  
   return (DataA)
 }
+
 
 xl = iris[,3:5];
 DataA = LOO(xl, q=0.2);  
