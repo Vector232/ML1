daaed0bb116cce5ac3db9dc86d1f38bcbd5a7286
Vector232 <71130074+Vector232@users.noreply.github.com>
2020-10-17 16:31:06 +0300

    Update PF.R

--- PF/PF.R
+++ PF/PF.R
@@ -39,15 +39,20 @@ core_3 = function(r){
     return(0)
 }
 
+core_4 = function(r)
+{
+  return (((2*pi)^(-1/2)) * exp(-1/2*r^2)) 
+}
+
 classif_PF = function(xl, pots, h, orderedXl, core = core_1) {
-  l = nrow(xl);
-  n = ncol(xl);
+  l = nrow(xl)
+  n = ncol(xl)
   
-  weights = table(xl[, n]);
-  weights[1:dim(weights)] = 0;
+  weights = table(xl[, n])
+  weights[1:dim(weights)] = 0
   
   for (i in 1:l) {
-    weights[xl[i, n]] = weights[xl[i, n]] + (pots[i] * core(orderedXl[i] / h));
+    weights[xl[i, n]] = weights[xl[i, n]] + (pots[i] * core(orderedXl[i] / h))
   }
   if (max(weights) != 0)
     return (which.max(weights))
@@ -56,48 +61,50 @@ classif_PF = function(xl, pots, h, orderedXl, core = core_1) {
 }
 
 PF = function(xl, pots, h, z){
-  return(classif_PF(xl, pots, h, sortObjectsByDist(xl, z)));
+  return(classif_PF(xl, pots, h, sortObjectsByDist(xl, z)))
 }
 
-SetPots = function(xl, err){
+SetPots = function(xl, err,h){
   l = nrow(xl)
   n = ncol(xl)
-  error = err + 1
+  error = 999
+  nerror = 0;
   pots = array(0,l)
-  distances = matrix(-1,l,l)
+  distances = matrix(-1,l,l-1)
   
   for(i in 1:l){
-    distances[i,] = sortObjectsByDist(xl, c(xl[i,1],xl[i,2]))
+    distances[i,] = sortObjectsByDist(xl[-i,], c(xl[i,1],xl[i,2]))
     print(i)
   }
-  
-  while(error > err){
-    while(TRUE){
-      i = sample(1:l, 1);
-      
-      class = classif_PF(xl, pots, h=1, distances[i,]);
+    for(i in 1:l){
+      class = classif_PF(xl[-i,], pots, h, distances[i,])
       
       if(class != c(xl[i,n])){
-        pots[i] = pots[i] + 1;
-        break;
-      }
-      error = 0;
-      for(j in 1:l){
-        class = classif_PF(xl[-j,], pots, h=1, distances[j,]);
-        if(class != c(xl[j,n])){
-          error = error + 1;
+        pots[i] = pots[i] + 1
+        
+        nerror = 0;
+        for(j in 1:l){
+          class = classif_PF(xl[-j,], pots, h, distances[j,])
+          if(class != c(xl[j,n])){
+            nerror = nerror + 1
+          }
+        }
+        
+        if(nerror>=error){
+          pots[i] = pots[i] - 1
+        }
+        else if(nerror <= err){
+          i = 1
+        }
+        else{
+          error = nerror
+          e = paste("error = ", error);
+          print(e)
+          print(pots)
         }
       }
-    }
-    e = paste("error = ", error);
-    print(e)
-    print(pots)
+     }
   }
-  print("---------------------------")
-  print(pots)
-  return(pots)
-}
-
 
 
 
