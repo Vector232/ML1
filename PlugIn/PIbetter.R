6a7a0871344ba23558b5b9ec6de05ecd55e3ce80
Vector232 <71130074+Vector232@users.noreply.github.com>
2020-11-20 10:45:05 +0300

    Update PIbetter.R

--- PlugIn/PIbetter.R
+++ PlugIn/PIbetter.R
@@ -11,7 +11,7 @@ f = function(x, classes, aprior, mus, covs, lyambda) {
   n = ncol(mus)
   scores = rep(0, l)
   for (i in 1:l) {
-    scores[i] = lyambda[i] * aprior[i] * fprob(x, mus[i,], covs[[i]])
+    scores[i] = exp(lyambda[i] * aprior[i]) * fprob(x, mus[i,], covs[[i]])
   }
   
   ans = which.max(scores)
@@ -51,6 +51,35 @@ PI = function(xl){
   
   
   classificationmap(function(x) f(x, classes, aprior, mus, covar, lyambda), xl)
+  gline(xl, covar, mu, mus)
+}
+
+gline = function(xl, covar, mu, mus){
+ Y = length(unique(xl[,3]))
+ x = seq(1,7,length.out=100)
+ y = seq(0,3,length.out=100)
+ for (i in 1:Y) {
+   for (j in (i+1):Y) {
+     if (j > Y) break
+     
+     form = function(x,y,mu,mat) {
+       a = mat[1,1]
+       b = mat[1,2]
+       c = mat[2,1]
+       d = mat[2,2]
+       a*x*x + (b+c)*x*y + d*y*y + 
+       (-2*a*mu[1]-b*mu[2]-c*mu[2])*x +
+       (-b*mu[1]-c*mu[1]-2*d*mu[2])*y +
+       (a*mu[1]*mu[1]+(b+c)*mu[1]*mu[2]+d*mu[2]*mu[2])
+     }
+     
+     z = outer(x,y,function(x,y) 
+      form(x,y,mus[i,],solve(covar[[i]])) -
+       form(x,y,mus[j,],solve(covar[[j]]))
+     )
+     contour(x,y,z,levels=0,add=T, col = "red", lwd = 3)
+   }
+ }
 }
 
 classificationmap = function(classifier, xl) {
